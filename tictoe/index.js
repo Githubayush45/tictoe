8dbaceb5d0b2c59e83cfadbd4602e73fe6c75f08
ayush <ayuhs@gmail.com>
2024-03-13 03:36:15 +0530

    4thcommit

--- tictoe/index.js
+++ tictoe/index.js
@@ -1,4 +1,30 @@
-console.log(1)
-bhavanaaaaaaa
-ayush 
-singh
+window.addEventListener('DOMContentLoaded', () => {
+const columns = Array.from(document.querySelectorAll('.column'));
+const game2 = document.querySelector('.game2');
+const resetButton = document.querySelector('#reset');
+const game4 = document.querySelector('.game4');
+
+let board=['','','','','','','',''];
+let current='X';
+let ongoinggame=true;
+
+const playerx='playerx';
+const playero='playero';
+const draw='draw';
+
+const winningarea=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+];
+column,forEach(column) => {
+    column.addEventListener("click",()=>){
+        console.log("")
+    };
+};
+});
